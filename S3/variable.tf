38853c2583d8edd1db0efb216c00f7f0f45b9120
Anjiraju998 <107533726+Anjiraju998@users.noreply.github.com>
2023-02-22 16:35:44 +0530

    Update variable.tf

--- S3/variable.tf
+++ S3/variable.tf
@@ -1,17 +1,13 @@
 variable "B_name" {
   type = string
-  default = "aws-b-test998"
 }
 variable B_tags {
   type = string
-  default = "my_bucket"
 }
 variable "B_env" {
   type = string
-  default = "Dev"
 }
 variable "B_acl" {
   type = string
-  default = "private"
 }
 
