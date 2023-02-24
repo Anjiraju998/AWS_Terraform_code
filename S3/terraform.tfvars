a7b663f3db74c3a5201058eae92ea471e4e17d75
Anjiraju998 <107533726+Anjiraju998@users.noreply.github.com>
2023-02-24 16:45:09 +0530

    Add files via upload

---
+++ S3/terraform.tfvars
@@ -0,0 +1,4 @@
+B_name = "aws-b-test998"
+B_tags = "my_bucket"
+B_env = "Dev"
+B_acl = "private"
---
+++ S3/variable.tf
@@ -0,0 +1,13 @@
+variable "B_name" {
+  type = string
+}
+variable B_tags {
+  type = string
+}
+variable "B_env" {
+  type = string
+}
+variable "B_acl" {
+  type = string
+}
+
